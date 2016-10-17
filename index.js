f3c703aa53459d58305be4e4f9b8d41dd440af07
HcgRandon <randon@dvfl.io>
2016-10-17 10:28:46 -0400

    Using a real lib

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-var config = require("./config.json"),
-    Eris = require("eris"),
-    bot = new Eris(`Bot ${config.token}`),
-    request = require("request");
+var config    = require("./config.json");
+var Discordie = require("discordie");
+var bot       = new Discordie();
 
-bot.on("ready", () => {
+var request   = require("request");
+
+bot.connect({'token': config.token});
+
+bot.Dispatcher.on("GATEWAY_READY", e => {
     console.log("Ready!");
 });
 
-bot.on("messageCreate", (msg) => {
-    if(msg.content.indexOf(`${config.prefix}achievement`) == 0) {
-      let ach_text = msg.content.split(`${config.prefix}achievement `)[1];
-      var url = `https://www.minecraftskinstealer.com/achievement/a.php?i=2&h=${config.header}&t=${ach_text}`;
-      request({url: url, encoding: null}, (err, response, body) => {
-        if (err) { bot.createMessage(msg.channel.id, "Error: " + err) }
-        if (response.statusCode != 200) { bot.createMessage(msg.channel.id, "Got status code " + response.statusCode) }
-        bot.createMessage(msg.channel.id, `Created by **${msg.author.username}**`, {
-          file: response.body,
-          name: 'gratz.png'
-        });
-      });
-    }
+bot.Dispatcher.on("MESSAGE_CREATE", e => {
+ var msg = e.message;
+ if(msg.content.indexOf(`${config.prefix}achievement`) == 0) {
+  let ach_text = msg.content.split(`${config.prefix}achievement `)[1];
+  var url = `https://www.minecraftskinstealer.com/achievement/a.php?i=2&h=${config.header}&t=${ach_text}`;
+  request({url: url, encoding: null}, (err, response, body) => {
+   if (err) { bot.createMessage(msg.channel.id, "Error: " + err) }
+   if (response.statusCode != 200) { bot.createMessage(msg.channel.id, "Got status code " + response.statusCode) }
+   bot.createMessage(msg.channel.id, `Created by **${msg.author.username}**`, {
+    file: response.body,
+    name: 'gratz.png'
+   });
+  });
+ }
 });
 
 bot.connect();
